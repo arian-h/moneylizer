a2cc524e69c5f7229467cfe2c92f2eaacdecead6
arian-h <aryan.hosseinzadeh@gmail.com>
2018-08-01 01:21:23 -0700

    Initial commit: Created initial spring+gradle project

---
+++ src/main/java/com/karen/moneylizer/MoneylizerApplication.java
@@ -0,0 +1,12 @@
+package com.karen.moneylizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneylizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneylizerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/karen/moneylizer/MoneylizerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.karen.moneylizer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoneylizerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
