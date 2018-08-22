e5565a0b0ac50e231c0c533b914816cc993637ab
arian-h <aryan.hosseinzadeh@gmail.com>
2018-08-21 23:12:26 -0700

    Deleted files related to the investment entity + spring configuration metadata

--- src/main/java/com/karen/moneylizer/investment/repository/InvestmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.karen.moneylizer.investment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.karen.moneylizer.investment.model.InvestmentEntity;
-
-public interface InvestmentRepository extends
-		JpaRepository<InvestmentEntity, Long> {
-}
