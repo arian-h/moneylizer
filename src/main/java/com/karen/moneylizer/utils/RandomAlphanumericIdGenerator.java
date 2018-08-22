1d81ed578d1afdf2cdb031ceec8e5f2be1ef14dc
arian-h <aryan.hosseinzadeh@gmail.com>
2018-08-22 00:20:34 -0700

    Removed id prefix from the random alphanumeric id generator

--- src/main/java/com/karen/moneylizer/utils/RandomAlphanumericIdGenerator.java
+++ src/main/java/com/karen/moneylizer/utils/RandomAlphanumericIdGenerator.java
@@ -1,7 +1,6 @@
 package com.karen.moneylizer.utils;
 
 import java.io.Serializable;
-import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.lang3.RandomStringUtils;
@@ -13,29 +12,20 @@ import org.hibernate.id.IdentifierGenerator;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.type.Type;
 
-import com.google.common.collect.ImmutableMap;
-
 public class RandomAlphanumericIdGenerator implements IdentifierGenerator,
 		Configurable {
 
-	public static final String generatorName = "myGenerator";
-	private String id_prefix;
-
-	static final Map<String, String> ID_PREFIX_MAP = ImmutableMap
-			.<String, String> builder()
-			.put("com.karen.moneylizer.investment.model.InvestmentEntity",
-					"INV_").build();
+	public static final String generatorName = "RandomAlphanumericIdGenerator";
 
 	@Override
 	public Serializable generate(SharedSessionContractImplementor session,
 			Object object) throws HibernateException {
-		return id_prefix + RandomStringUtils.randomAlphanumeric(10);
+		return RandomStringUtils.randomAlphanumeric(10);
 	}
 
 	@Override
 	public void configure(Type type, Properties params,
-			ServiceRegistry serviceRegistry) throws MappingException {
-		id_prefix = ID_PREFIX_MAP.get(params.getProperty("entity_name"));
+			ServiceRegistry serviceRegistry) throws MappingException {		
 	}
 
 }
